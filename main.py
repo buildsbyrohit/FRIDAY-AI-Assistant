bf3240491ed5c2e24edc0f0c39bab2d403dc9a55
buildsbyrohit <hinduraorohit11@gmail.com>
2026-08-20 12:31:28 +0530

    Day 3: Add application control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime as dt 
 import webbrowser as wb
+import subprocess
 print("FRIDAY IS ONLINE")
 def greet():
     print("hello sir")
@@ -14,12 +15,27 @@ def date_time():
     current_time = dt.datetime.now()
     format_time = current_time.strftime("%I:%M %p")
     print(f"current time is {format_time}")
+
+def open_application(application):
+    process = applications.get(application)
+    if process is not None:
+        
+        subprocess.run(process)
+    else:
+        print("no path for this yet")
+
+
 commands = {
     "hello": greet,
     "status":status, 
     "time": date_time, 
     "exit": shutdown,
     }
+
+applications = {
+    "calculator": r"C:\Windows\System32\calc.exe",
+    "notepad": r"C:\Windows\notepad.exe"
+}
 
 while True:
     
@@ -31,8 +47,10 @@ while True:
     
     functions = commands.get(lower_query)
     if len(split_query) == 2:
-        if split_query[0] == "open":
-
+        if split_query[0] == "open" and split_query[1] in applications:
+                open_application(split_query[1])
+                continue
+        elif split_query[0] == "open": 
             open_website(split_query[1])
             continue
         else:
@@ -45,7 +63,3 @@ while True:
     else:
         print("i dont know this cmd yet")
     
-    
-
-    
-
